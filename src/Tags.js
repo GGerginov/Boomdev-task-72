e90fd9c5147ed960a4af729835892dfdf8f52a07
GGerginov <electro44@abv.bg>
2022-12-24 16:45:31 +0200

    Finish 3

--- src/Tags.js
+++ src/Tags.js
@@ -2,15 +2,10 @@ import Tag from "./Tag";
 
 export function Tags(props) {
 
-
   return <div className="tags">
-    <p className='tag'>#{props.tags[0]}</p>
-    <p className='tag'>#{props.tags[1]}</p>
-    <p className='tag'>#{props.tags[2]}</p>
-    <p className='tag'>#{props.tags[3]}</p>
-    {/*<Tag tagName = {props.tags[0]}/>*/}
-    {/*<Tag tagName = {props.tags[1]}/>*/}
-    {/*<Tag tagName = {props.tags[2]}/>*/}
-    {/*<Tag tagName = {props.tags[3]}/>*/}
+    <Tag tagName = {props.tags[0]}/>
+    <Tag tagName = {props.tags[1]}/>
+    <Tag tagName = {props.tags[2]}/>
+    <Tag tagName = {props.tags[3]}/>
   </div>;
 }
